58f2f1b714154cd57057822657e77c605b434b6b
Anya <anyaazariah@gmail.com>
2024-03-18 17:29:03 -0700

    add raw sapphire block

--- src/main/java/net/anya/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/anya/tutorialmod/block/ModBlocks.java
@@ -31,4 +31,7 @@ public class ModBlocks {
     }
     public static final RegistryObject <Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
             ()-> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    public static final RegistryObject <Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
+            ()-> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
 }
